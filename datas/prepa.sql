82be5c00a21ca0202ba8ffebdfcfc3ae4c3c148d
jeanbapt56 <toutafela@gmail.com>
2019-03-15 14:16:15 +0100

    text

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -4,4 +4,7 @@ SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
 
-SELECT thetitle, thetext, thename, thedate, iduser, idarticle FROM article INNER JOIN user ON  user.iduser = article.idarticle;
+SELECT thetitle, thetext, thename, thedate, iduser, idarticle FROM article 
+INNER JOIN user ON  user.iduser = article.idarticle 
+INNER JOIN article on article.idarticle = categorie_idcategorie 
+INNER JOIN categorie_has_article on categorie_has_article.categorie_idcategorie = categorie.idcategorie;
